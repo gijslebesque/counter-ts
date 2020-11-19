9989a94e5b427c81f228b8e2c1edd4f6f93ca712
gijslebesque <gijslebesque@gmail.com>
2020-11-19 15:53:45 +0100

    rm logo

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Logo from '../images/tmi-logo.png';
 
 export default function Navigation() {
   return (
     <nav className="navigation flex flex-align-left">
-      <img src={Logo} alt="Logo" className="logo" />
+      <div className="logo">LOGO</div>
     </nav>
   );
 }
